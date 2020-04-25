d11076cf697dacecddbeb450245b7fd3b1255068
weihua.huang <weihua.huang@hiscene.com>
2020-04-26 01:28:41 +0800

    1

--- jwtService/jwtService.go
+++ jwtService/jwtService.go
@@ -14,9 +14,6 @@ import (
 * @Date: 2020/4/24 22:36
  */
 
-const LOGIN_TOKEN_KEY = "sysUser:token_%s";
-const INVALID_TOKEN_KEY = "sysUserTokenInvalid:%s"
-
 type UserClaims struct {
 	UserId uint `json:"user_id"`
 	UserName string `json:"username"`
@@ -26,19 +23,26 @@ type UserClaims struct {
 	jwt.StandardClaims
 }
 
-func CreateToken(claims UserClaims) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(config.ConfigData.JwtEncrtpy))
+type UserJwt struct{
+	Type 		string	//用户类型
+	Encrtpy 	string	//密钥
+	TokenKey 	string	//token索引
+	InValidTokenKey string	//失效token索引
 }
 
-func ParseToken(tokenStr string) (*UserClaims, error) {
+func (user *UserJwt)CreateToken(claims UserClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(user.Encrtpy))
+}
+
+func (user *UserJwt)ParseToken(tokenStr string) (*UserClaims, error) {
 
 	token, err := jwt.ParseWithClaims(tokenStr, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 
-		return []byte(config.ConfigData.JwtEncrtpy), nil
+		return []byte(user.Encrtpy), nil
 	})
 
 	if err != nil {
@@ -53,31 +57,31 @@ func ParseToken(tokenStr string) (*UserClaims, error) {
 	return nil, err
 }
 
-func GetExistsToken(userName string)(string, error){
-	return redis.Redis.Get(CreateTokenKey(userName)).Result()
+func (user *UserJwt)GetExistsToken(userName string)(string, error){
+	return redis.Redis.Get(user.CreateTokenKey(userName)).Result()
 }
 
-func SetToken(userName string, token string) error{
-	return redis.Redis.Set(CreateTokenKey(userName), token, time.Minute * time.Duration(config.ConfigData.LoginExpire)).Err()
+func (user *UserJwt)SetToken(userName string, token string) error{
+	return redis.Redis.Set(user.CreateTokenKey(userName), token, time.Minute * time.Duration(config.ConfigData.LoginExpire)).Err()
 }
 
-func DelToken(userName string){
-	redis.Redis.Del(CreateTokenKey(userName))
+func (user *UserJwt)DelToken(userName string){
+	redis.Redis.Del(user.CreateTokenKey(userName))
 }
 
 // token索引键
-func CreateTokenKey(userName string) (string){
-	return fmt.Sprintf(LOGIN_TOKEN_KEY, userName)
+func (user *UserJwt)CreateTokenKey(userName string) (string){
+	return fmt.Sprintf(user.TokenKey, userName)
 }
 
 // 加入到失效token列表
-func JoinInvalidToken(token string){
-	redis.Redis.Set(fmt.Sprintf(INVALID_TOKEN_KEY, token), token, time.Minute * time.Duration(config.ConfigData.LoginExpire))
+func (user *UserJwt)JoinInvalidToken(token string){
+	redis.Redis.Set(fmt.Sprintf(user.InValidTokenKey, token), token, time.Minute * time.Duration(config.ConfigData.LoginExpire))
 }
 
 // token是否已失效
-func IsInvalidToken(token string) bool{
-	_, err := redis.Redis.Get(fmt.Sprintf(INVALID_TOKEN_KEY, token)).Result()
+func (user *UserJwt)IsInvalidToken(token string) bool{
+	_, err := redis.Redis.Get(fmt.Sprintf(user.InValidTokenKey, token)).Result()
 	if err == redis2.Nil{
 		return false
 	}
